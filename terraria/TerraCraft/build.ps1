e95164249d9ab8020f512d8fba2f4693461f8b89
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-03-11 18:13:00 +0000

    Make the powershell script only build for windows

--- terraria/TerraCraft/build.ps1
+++ terraria/TerraCraft/build.ps1
@@ -1,22 +1,7 @@
-#!/usr/bin/env pwsh
-
-$os = ""
-if ($env:OS -eq "Windows_NT") {
-    $os = "win"
-}
-elseif ($IsLinux) {
-    $os = "linux"
-}
-else {
-    Write-Host "Unsupported OS"
-    exit 1
-}
-
-dotnet publish -c Release -r $os-x64 --self-contained true # -p:PublishTrimmed=true 
-
+dotnet publish -c Release -r win-x64 --self-contained true # -p:PublishTrimmed=true 
 
 New-Item -ItemType Directory -Force -Path build 
 
-$path = "TerraCraft/bin/Release/net6.0/$os-x64/publish"
+$path = "TerraCraft/bin/Release/net6.0/win-x64/publish"
 
 Move-Item -Path "$path/Grpc.Core.Api.dll", "$path/Grpc.Net.Client.dll", "$path/Grpc.Net.Common.dll", "$path/Microsoft.Extensions.Logging.Abstractions.dll", "$path/TerraCraft.dll" -Destination "build" -Force
