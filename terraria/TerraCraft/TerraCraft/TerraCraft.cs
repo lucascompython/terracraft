866b00d9296f9de3c205c44089ff690487e0c4ad
Lucas Linhares <lucasdelinhares@gmail.com>
2024-03-16 18:06:53 +0000

    Make the terraria plugin read a config file

--- terraria/TerraCraft/TerraCraft/TerraCraft.cs
+++ terraria/TerraCraft/TerraCraft/TerraCraft.cs
@@ -4,38 +4,64 @@ using TerrariaApi.Server;
 using Grpc.Core;
 using Grpc.Net.Client;
 using Microsoft.Xna.Framework;
+using System.Text.Json;
 
 namespace TerraCraft
 {
+
+    public struct Config
+    {
+        public int MinecraftServerPort { get; set; }
+        public string? MinecraftServerAddress { get; set; }
+        public string? Token { get; set; }
+    }
+
     [ApiVersion(2, 1)]
-    public class TerraCraft : TerrariaPlugin
+    public sealed class TerraCraft : TerrariaPlugin
     {
         public override string Name => "TerraCraft";
         public override Version Version => new(1, 0);
         public override string Author => "Lucas de Linhares";
         public override string Description => "A plugin that makes a chat bridge with a Minecraft Server";
 
+        private readonly string _configPath = Path.Combine(TShock.SavePath, "terracraft_config.json");
+        private Config config;
 
         private GrpcChannel grpcChannel;
-        AsyncDuplexStreamingCall<ChatMessage, ChatMessage> chat;
+        private AsyncDuplexStreamingCall<ChatMessage, ChatMessage> chat;
 
         public TerraCraft(Main game) : base(game)
         {
+            if (!File.Exists(_configPath))
+            {
+                config = new Config
+                {
+                    MinecraftServerPort = 50051,
+                    MinecraftServerAddress = "http://localhost",
+                    Token = "your_token_here"
+                };
 
+                File.WriteAllText(_configPath, JsonSerializer.Serialize(config, new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                }));
+            }
+            else
+            {
+                config = JsonSerializer.Deserialize<Config>(File.ReadAllText(_configPath));
+            }
         }
 
         public override void Initialize()
         {
-
-            grpcChannel = GrpcChannel.ForAddress("http://localhost:50051", new GrpcChannelOptions
+            grpcChannel = GrpcChannel.ForAddress($"{config.MinecraftServerAddress}:{config.MinecraftServerPort}", new GrpcChannelOptions
             {
-                Credentials = ChannelCredentials.Insecure
+                Credentials = ChannelCredentials.Insecure,
             });
             var chatServiceClient = new ChatService.ChatServiceClient(grpcChannel);
             chat = chatServiceClient.Chat();
 
             ServerApi.Hooks.ServerChat.Register(this, OnChat);
-
             Task.Run(ReadMessage);
         }
 
